b03220712b6e80a6e507bd7d9fde67da233715ac
ohaddahan <ohaddahan@gmail.com>
2025-08-10 13:37:03 +0300

    Update python.sh

--- python.sh
+++ python.sh
@@ -5,11 +5,10 @@ apt install python3.12-venv
 #
 git clone https://github.com/fpgaminer/joycaption.git
 #
-pip config set global.cache-dir /workspace/pip-cache
 python3 -m venv .venv
 source .venv/bin/activate
 pip config set global.cache-dir /workspace/pip-cache
 pip install -U pip
-pip install torch torchvision torchaudio
-pip install tensorflow
+#pip install torch torchvision torchaudio
+#pip install tensorflow
 pip install -r requirements.txt
