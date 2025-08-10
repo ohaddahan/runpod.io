220e7f6b7cc4cea92fdbb3b123af2457d7c61744
ohaddahan <ohaddahan@gmail.com>
2025-08-10 13:22:17 +0300

    Update python.sh

--- python.sh
+++ python.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 #
 apt-get install python3.11
+apt install python3.12-venv
 #
 git clone https://github.com/fpgaminer/joycaption.git
 #
-# python3 -m venv .venv
-# source .venv/bin/activate
-# pip install -U pip
-# pip install torch torchvision torchaudio
-# pip install tensorflow tensorflow-macos tensorflow-metal
-# pip install jax-metal ml_dtypes==0.2.0 jax==0.4.26 jaxlib==0.4.26
+pip config set global.cache-dir /workspace/pip-cache
+python3 -m venv .venv
+source .venv/bin/activate
+pip config set global.cache-dir /workspace/pip-cache
+pip install -U pip
+pip install torch torchvision torchaudio
+pip install tensorflow
+pip install -r requirements.txt
