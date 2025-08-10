ceb009e90208494757a04b215d6db79fd3d6d902
ohaddahan <ohaddahan@gmail.com>
2025-08-10 13:06:12 +0300

    Create aliases.sh

---
+++ aliases.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "alias lt='ls -lrtha --color'" >> ~/.bashrc
+echo "alias grep='grep --color'" >> ~/.bashrc
+echo "alias c='clear'" >> ~/.bashrc
+echo "alias s='source'" >> ~/.bashrc
+echo "alias gitstatus='git status'" >> ~/.bashrc
+echo "alias cp='cp -i'" >> ~/.bashrc
+echo "alias rm='rm -i'" >> ~/.bashrc
+echo "alias h='history'" >> ~/.bashrc
