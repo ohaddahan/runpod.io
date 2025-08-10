8986268ee8cefff29565085a0819c2cb7b26ca35
ohaddahan <ohaddahan@gmail.com>
2025-08-10 13:11:36 +0300

    Create python.sh

---
+++ python.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+apt-get install python3.11
+#
+git clone https://github.com/fpgaminer/joycaption.git
+#
+# python3 -m venv .venv
+# source .venv/bin/activate
+# pip install -U pip
+# pip install torch torchvision torchaudio
+# pip install tensorflow tensorflow-macos tensorflow-metal
+# pip install jax-metal ml_dtypes==0.2.0 jax==0.4.26 jaxlib==0.4.26
