34f0dc5371c1e34ceb3dfe4ff6177b64f2925cce
ohaddahan <ohaddahan@gmail.com>
2025-08-11 13:30:07 +0300

    Update python.sh

--- python.sh
+++ python.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+# git clone https://github.com/fpgaminer/joycaption.git
+
 export HF_HOME=/workspace/hf-home
 export HF_HUB_CACHE=/workspace/hf-hub-cache
 apt-get update
 apt-get install python3.11 -y
 apt install python3.12-venv -y
 apt install vim -y
-
-git clone https://github.com/fpgaminer/joycaption.git
+pip config set global.cache-dir /workspace/pip-cache
 
 python3 -m venv .venv
 source .venv/bin/activate
